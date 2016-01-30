9018e2e10ba74081f1072ccb51829d4042c13212
mokolodi1 <mokolodi1@gmail.com>
2016-01-30 13:58:57 -0800

    bugfixes

--- development.sh
+++ development.sh
@@ -1,5 +1,5 @@
 export MONGO_URL="mongodb://localhost:27017/MedBook"
 
 if [ -z "$1" ]; then
-    meteor --settings ~/work/sms-sender/settings.json
+    meteor --settings ~/work/smsr/settings.json
 fi
